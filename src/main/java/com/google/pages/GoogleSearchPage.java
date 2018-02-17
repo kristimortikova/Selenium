0f2e5a5ed3e434a985f656b203bc37002034253f
kristimortikova <kristimortikova@gmail.com>
2018-02-17 11:44:48 +0200

    add annotation Attachment

--- src/main/java/com/google/pages/GoogleSearchPage.java
+++ src/main/java/com/google/pages/GoogleSearchPage.java
@@ -1,5 +1,6 @@
 package com.google.pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.How;
 public class GoogleSearchPage {
     @FindBy(how = How.XPATH, using = "//*[@id=\"lst-ib\"]")
     private WebElement searchField;
+    @Step("Enter data {0}")
     public void searchFor(String searchData){
         searchField.sendKeys(searchData);
         searchField.submit();
