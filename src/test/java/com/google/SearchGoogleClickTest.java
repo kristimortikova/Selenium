5ae01fdcbac56b034139cab187c32358a3aeffda
kristimortikova <kristimortikova@gmail.com>
2018-02-17 12:04:49 +0200

    add google click test

--- src/test/java/com/google/SearchGoogleClickTest.java
+++ src/test/java/com/google/SearchGoogleClickTest.java
@@ -31,7 +31,7 @@ public class SearchGoogleClickTest extends WebDriverTestBase {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }*/
 
-    @Test(enabled = false)
+    @Test
     public void searchGoogleTest(){
         driver.get("https://www.google.ua");
         searchField = driver.findElement(searchFieldLocator);
