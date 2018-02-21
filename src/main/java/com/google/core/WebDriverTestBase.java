7586709f8189e4136fce05d1a985bd3dd3201715
kristimortikova <kristimortikova@gmail.com>
2018-02-21 19:34:48 +0200

    conf.xml and to testBase add desired capabilities

--- src/main/java/com/google/core/WebDriverTestBase.java
+++ src/main/java/com/google/core/WebDriverTestBase.java
@@ -6,28 +6,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 @Listeners({com.google.core.TestListener.class})
 public class WebDriverTestBase {
     protected WebDriver driver;
     private String browser = System.getProperty("browser", "CHROME");
-    //WebDriverManager manager;
+    private DesiredCapabilities setDesiredCapabilities(String platform, String remoteBrowser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        if (platform.equalsIgnoreCase(Platform.WIN10.name())) {
+            caps.setPlatform(Platform.WIN10);
+            caps.setBrowserName(remoteBrowser);
+        }
+        return caps;
+    }
+    @Parameters({"platform", "remoteBrowser"})
     @BeforeClass
-    public void setUp() {
-        //WebDriverManager.chromedriver().setup();
-        //driver = new ChromeDriver();
+    public void setUp(@Optional String platform, @Optional String remoteBrowser)throws MalformedURLException {
             switch (browser){
             case "CHROME":
-                //manager.chromedriver().setup();
                 ChromeDriverManager.getInstance().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--headless");
@@ -38,6 +46,10 @@ public class WebDriverTestBase {
                 FirefoxOptions options = new FirefoxOptions();
                 options.addArguments("--headless");
                 driver = new FirefoxDriver(options);
+                break;
+            case "remote":
+                DesiredCapabilities caps = setDesiredCapabilities(platform, remoteBrowser);
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
                 break;
         }
         driver.manage().window().maximize();
